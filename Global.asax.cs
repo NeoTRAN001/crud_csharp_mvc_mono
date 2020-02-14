2f8b969f15355cd77140c9afd4085830fbcd0c47
NeoTRAN <neo_tran@outlook.com>
2020-02-13 18:24:43 -0600

    Se ha agregado el archivo global donde viene la conexion a postgres

---
+++ Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using CRUD.Models;
+
+namespace CRUD
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            DataAccess.StrConnection = "Host=localhost;" +
+            	                       "Port=5432;" +
+            	                       "Username=postgres;" +
+            	                       "Password=qwertypy;" +
+            	                       "Database=Prueba;";
+        }
+    }
+}
